4fa5c74dc0486b69e4106e02fabfb347c0b885a1
Kamil Salawa <emir.salawa@gmail.com>
2021-07-15 10:02:20 +0100

    Initial Commit

---
+++ magicBall8Tests/magicBall8Tests.swift
@@ -0,0 +1,33 @@
+//
+//  magicBall8Tests.swift
+//  magicBall8Tests
+//
+//  Created by Kamil Salawa on 15/07/2021.
+//
+
+import XCTest
+@testable import magicBall8
+
+class magicBall8Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
