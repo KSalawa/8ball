3de0c47994a091f4734a0c6b2b29452dcd763abd
Kamil Salawa <emir.salawa@gmail.com>
2021-07-15 16:25:17 +0100

    finalised app

--- magicBall8/ViewController.swift
+++ magicBall8/ViewController.swift
@@ -6,14 +6,40 @@
 //
 
 import UIKit
+import AVFoundation
+
 
 class ViewController: UIViewController {
-
+    
+    var player: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    var ball = [#imageLiteral(resourceName: "ball1"), #imageLiteral(resourceName: "ball2"), #imageLiteral(resourceName: "ball3"), #imageLiteral(resourceName: "ball4"), #imageLiteral(resourceName: "ball5"), #imageLiteral(resourceName: "ball5")]
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+    @IBAction func shakeButton(_ sender: UIButton) {
+        playSound()
+        imageView.image = ball.randomElement()!
+        //print(imageView.image!)
+        
+        let impactMed = UIImpactFeedbackGenerator(style: .rigid)
+            impactMed.impactOccurred()
+        
+            }
+    func playSound() {
+        let url = Bundle.main.url(forResource: "sound", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
+
 
